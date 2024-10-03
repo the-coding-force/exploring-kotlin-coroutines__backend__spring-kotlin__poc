84eb23d86df9f262f69f47857692bc16002583ec
Adriano Santos <adrianosantosx2@hotmail.com>
2024-10-03 19:39:58 -0300

    second commit

---
+++ src/main/kotlin/the/coding/force/exploring_kotlin_coroutines/ExploringKotlinCoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package the.coding.force.exploring_kotlin_coroutines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExploringKotlinCoroutinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExploringKotlinCoroutinesApplication>(*args)
+}
---
+++ src/test/kotlin/the/coding/force/exploring_kotlin_coroutines/ExploringKotlinCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package the.coding.force.exploring_kotlin_coroutines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExploringKotlinCoroutinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
