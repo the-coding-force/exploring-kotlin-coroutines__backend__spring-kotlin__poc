a538f44441da734e7eb396ea0738d05dec63b364
Lucas Terra <lucasterracontato@gmail.com>
2024-12-12 10:53:18 -0300

    swagger for data class - refs #22 (T6)

--- src/main/kotlin/the/coding/force/exploring_kotlin_coroutines/request/BulkCreateDataRequest.kt
+++ src/main/kotlin/the/coding/force/exploring_kotlin_coroutines/request/BulkCreateDataRequest.kt
@@ -1,8 +1,21 @@
 package the.coding.force.exploring_kotlin_coroutines.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import the.coding.force.exploring_kotlin_coroutines.enums.DataStatusEnum
 
+@Schema(
+    description = "A DTO (data transfer object) used to send on request " +
+        "for save several entities how load test"
+)
 data class BulkCreateDataRequest(
+    @Schema(
+        description = "Quantity of entities to save in database",
+        example = "500"
+    )
     val quantity: Int,
+
+    @Schema(
+        description = "A Enum that represents the status of task"
+    )
     val status: DataStatusEnum?
 )
